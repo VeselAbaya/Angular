87d510246a1ea5631cd2c1551f0e57c7688ec321
VeselAbaya <Vandervise465@gmail.com>
2017-11-09 18:29:05 +0300

    Добавил очень много всего, конфиг вроде тоже

---
+++ angular-courses/lecture 3/seed/src/app.component.ts
@@ -0,0 +1,12 @@
+
+import {Component} from "@angular/core";
+import {selector} from "rxjs/operator/publish";
+
+@Component({
+    selector: "app-component",
+    template: "<h1>This is my first app</h1>"
+    // templateUrl: "app.component.html"
+})
+export class AppComponent {
+
+}
---
+++ angular-courses/lecture 3/seed/src/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+
+import { Component } from "@angular/core";
+import {BrowserModule} from "@angular/platform-browser";
+import {AppComponent} from "./app.component";
+
+@NgModule({
+    imports: [BrowserModule],
+    exports: [AppComponent],
+    declaration: [],
+    providers: [],
+    bootstrap: []
+})
+
+export class AppModule {
+
+}
